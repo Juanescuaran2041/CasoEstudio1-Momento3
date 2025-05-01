fd643207a98dcac16fe17fe480a16f42e59653b2
Juanescuaran2041 <juanescuaransantander@gmail.com>
2025-05-01 10:48:47 -0500

    Agregando archivos del proyecto de notas
    Agregando archivos del proyecto de notas

---
+++ CasoEstudio1/Curso.py
@@ -0,0 +1,140 @@
+import random
+class Curso:
+   
+    TOTAL_EST = 12
+   
+    def __init__(self):
+        self.notas = []
+        self.LlenarNotas ()
+        
+    def LlenarNotas(self):
+        for est_Cont in range(Curso.TOTAL_EST):
+            # Generamos una nota aleatoria entre 1.0 y 5.0 y la añadimos a la lista
+            self.notas.append(round(random.uniform(4.0, 5.0),1))
+   
+   
+    def promedio (self):
+        promedio = 0
+        est = 0
+        while est < self.TOTAL_EST:
+            promedio += self.notas[est]
+            est +=1
+       
+        return promedio/self.TOTAL_EST
+   
+    def promedioFor (self):
+       
+        for nota in self.notas:
+            promedio +=nota
+       
+        return promedio/ self.TOTAL_EST
+   
+   
+    def notaMasRecurrente (self):
+        notaR:float = 0.0
+        Cantidad = 0
+       
+        for i in range (len(self.TOTAL_EST)):
+            notaBuscada = self.notas[i]
+            contador = 0
+            for j in range (len(self.TOTAL_EST)):
+                if self.notas [j] == notaBuscada:
+                    contador +=1
+           
+            if contador > Cantidad:
+                notaR = notaBuscada
+                Cantidad = contador
+               
+        return notaR
+   
+
+    __method__ = "CalcularMayorNota"
+    __params__ = "none"
+    __returns__ = "Null"
+    __description__ = "Metodo que sirve para calcular la mayor nota"
+    def CalcularMayorNota (self):
+        notaMayor = 0
+        for i in range (len(self.notas)):
+            self.notas [i]
+            if self.notas[i] > notaMayor:
+                notaMayor = self.notas [i]
+               
+        return notaMayor
+    
+    def mostrar_notas(self):
+        print(self.notas)
+                   
+    __method__ = "HacerCurva"
+    __params__ = "none"
+    __returns__ = "Null"
+    __description__ = "Metodo que sirve para aumentar las notas de los estudiantes en 5 porciento sin sobrepasarse de 5"
+
+    def HacerCurva(self):
+        for i in range(len(self.notas)):
+            nuevasNotas = round(self.notas[i] * 1.05, 1)  # Redondear a 1 decimal para facilitar la lectura de los datos
+            if nuevasNotas > 5:
+                nuevasNotas = 5
+            self.notas[i] = nuevasNotas
+
+    __method__ = "EncontrarPrimerasNotas"
+    __params__ = "none"
+    __returns__ = "Null"
+    __description__ = "Metodo que sirve para encontrar las primeras notas iguales a 1.5 y sobreescribirlas como 2.5"
+    def EncontrarPrimerasNotas(self):
+        Repeticion = 0
+        for i in range(len(self.notas)):
+            if self.notas[i] == 1.5 and Repeticion < 3:
+                self.notas[i] = 2.5
+                Repeticion += 1
+
+    __method__ = "EncontrarRepeticion5"
+    __params__ = "none"
+    __returns__ = "Posicion del tercer 5"
+    __description__ = "Metodo que sirve para encontrar la tercera vez que se repite la nota 5.0"
+
+    def EncontrarRepeticion5 (self):
+        Repeticion = 0
+        for i in range (len(self.notas)):
+            if self.notas [i] == 5.0: 
+                Repeticion +=1
+                if Repeticion == 3:
+                    return f'El valor 5 aparece por tercera vez en la posicion {i+1} del arreglo'
+           
+        
+        return -1
+
+    __method__ = "RemplazarNotas"
+    __params__ = "none"
+    __returns__ = "Null"
+    __description__ = "Metodo que sirve para sobreescribir las notas con 0.0 hasta que aparezca el primer 3"
+
+    def RemplazarNotas(self):
+        notas_copia = self.notas.copy() ##En esta parte hice una copia del atributo notas porque de lo contrario, los otros metodos se veian afectados con los nuevos valores
+        contador = 0
+
+        for i in range(len(notas_copia)):
+            if notas_copia[i] > 3.0:
+                contador += 1
+            if contador == 1:
+                notas_copia[i] = notas_copia[i] 
+            while contador == 0:
+                notas_copia[i] = 0.0
+                break
+
+            print(f"La nota {i+1} es {notas_copia[i]}")
+
+    __method__ = "calcularMinimoNotas3"
+    __params__ = "none"
+    __returns__ = "Null"
+    __description__ = "Metodo que sirve para saber la cantidad de notas minimas que se deben sumar para llegar a 30"
+
+    def calcularMinimoNotas3 (self):
+        suma = 0
+        for i in range(len(self.notas)):
+            suma +=self.notas [i]
+            if suma > 30:
+                return f'La cantidad minima de notas para lograr 30 es {i+1}'
+        return -1
+
+
+        
---
+++ CasoEstudio1/main.py
@@ -0,0 +1,57 @@
+from Curso import Curso
+
+if __name__ == "__main__":
+    curso = Curso()
+    
+    # Asignar notas para pruebas
+    curso.notas = [2.0, 2.0, 3.0, 5.0, 2.8, 5.0, 4.0, 4.2, 3.5, 5.0, 2.5, 3.0]
+
+    def mostrar_menu():
+        print("+----------------------------+")
+        print("|    MENÚ DE FUNCIONALIDAD   |")
+        print("+----------------------------+")
+        print("| 1. MostrarNotas            |")
+        print("| 2. RemplazarNotas          |")
+        print("| 3. EncontrarRepeticion5    |")
+        print("| 4. CalcularMinimoNotas30   |")
+        print("| 5. CalcularMayorNota       |")
+        print("| 6. Salir                   |")
+        print("+----------------------------+")
+
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            print("Notas ingresadas:")
+            curso.mostrar_notas()
+
+        elif opcion == "2":
+            print("\nHasta la primera nota mayor a 3 se remplazaran las anteriores con 0.0\n")
+            curso.RemplazarNotas()
+
+        elif opcion == "3":
+            resultado = curso.EncontrarRepeticion5()
+            print(f"\n{resultado}")
+
+        elif opcion == "4":
+            resultado = curso.calcularMinimoNotas3()
+            print(f"\n{resultado}")
+
+        elif opcion == "5":
+            mayor = curso.CalcularMayorNota()
+            print(f"\nLa mayor nota es: {mayor}")
+
+        elif opcion == "6":
+            print("Saliendo del programa...")
+            break
+
+        else:
+            print("Opción no válida. Intente nuevamente.")
+
+    ##A manera de aclaracion, si bien con el comando random se general numeros al azar, a veces no podra generar 3 veces el 5 por ejemplo
+    ## por ende, no se podria probar el metodo de encontrarRepetecion5, debido a esto, se opto por asignarlas manualmente
+
+    
+
+
